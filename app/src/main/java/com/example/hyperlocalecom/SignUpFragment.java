29650cea6798b997909836f3225cdd7a3133635e
varshii26 <varshijain2604@gmail.com>
2022-02-02 21:14:39 +0530

    Initial commit

--- app/src/main/java/com/example/hyperlocalecom/SignUpFragment.java
+++ app/src/main/java/com/example/hyperlocalecom/SignUpFragment.java
@@ -1,5 +1,7 @@
 package com.example.hyperlocalecom;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,11 +9,26 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +36,16 @@ import android.widget.TextView;
  * create an instance of this fragment.
  */
 public class SignUpFragment extends Fragment {
+
+    EditText mfullName, mphoneNo, memailId, mpassword, maddress;
+    Button mregisterBtn;
+    ImageButton closebtn;
+    ProgressBar pbar;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFireStore;
+
+
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.[a-z]+";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -67,8 +94,25 @@ public class SignUpFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View view =  inflater.inflate(R.layout.fragment_sign_up, container, false);
+
         alreadyHaveAnAccount = view.findViewById(R.id.goToLogin);
         parentFrameLayout = getActivity().findViewById(R.id.registerFrameLayout);
+
+
+        mfullName = view.findViewById(R.id.name);
+        mphoneNo = view.findViewById(R.id.phone);
+        memailId = view.findViewById(R.id.emailId);
+        mpassword = view.findViewById(R.id.password);
+        maddress = view.findViewById(R.id.address);
+
+        mregisterBtn = view.findViewById(R.id.registerBtn);
+        closebtn = view.findViewById(R.id.closeButton);
+
+        pbar = view.findViewById(R.id.signupProgressBar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFireStore = FirebaseFirestore.getInstance();
+
         return view;
     }
 
@@ -84,6 +128,121 @@ public class SignUpFragment extends Fragment {
 
 
         });
+
+
+        memailId.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                checkInputs();
+
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        });
+        mfullName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                checkInputs();
+
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        });
+        mphoneNo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                checkInputs();
+
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        });
+        maddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                checkInputs();
+
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        });
+        mpassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                checkInputs();
+
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        });
+
+
+
+
+
+        mregisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                checkEmail();
+
+            }
+        });
     }
 
         private void setFragment(Fragment fragment) {
@@ -93,5 +252,70 @@ public class SignUpFragment extends Fragment {
             fragmentTransaction.commit();
         }
 
+        private void checkInputs(){
+        if(!TextUtils.isEmpty(mfullName.getText())){
+            if(!TextUtils.isEmpty(memailId.getText())){
+                if(!TextUtils.isEmpty(mphoneNo.getText())){
+                    if(!TextUtils.isEmpty(maddress.getText())){
+                        if(!TextUtils.isEmpty(mpassword.getText()) && mpassword.length() >=8){
+                            mregisterBtn.setEnabled(true);
 
+                        }else{
+                            mregisterBtn.setEnabled(false);
+                        }
+                    }
+
+                }else{
+                    mregisterBtn.setEnabled(false);
+                }
+            }else{
+                mregisterBtn.setEnabled(false);
+
+            }
+        }else{
+            mregisterBtn.setEnabled(false);
+
+        }
+        }
+
+        private void checkEmail(){
+
+        if(memailId.getText().toString().matches(emailPattern)){
+
+            pbar.setVisibility(View.VISIBLE);
+            mregisterBtn.setEnabled(false);
+            firebaseAuth.createUserWithEmailAndPassword(memailId.getText().toString(),mpassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        FirebaseUser fuser = firebaseAuth.getCurrentUser();
+
+                      /* Intent mainIntent = new Intent(getActivity(),RegisterActivity.class);
+                        ((Activity) getActivity()).overridePendingTransition(0, 0);
+                       startActivity(mainIntent);
+                        // getActivity().finish();
+
+                       */
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        getActivity().startActivity(intent);
+
+                    }else{
+                        mregisterBtn.setEnabled(true);
+                        pbar.setVisibility(View.INVISIBLE);
+                        String error = task.getException().getMessage();
+                        Toast.makeText(getActivity(),error,Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            });
+
+
+        }else{
+
+            memailId.setError("Invalid Email!");
+
+        }
+
+        }
 }
