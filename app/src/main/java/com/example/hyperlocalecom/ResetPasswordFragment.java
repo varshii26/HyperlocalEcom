1f330d3357be7563f7e8f1e322ed3e165b544931
varshii26 <varshijain2604@gmail.com>
2022-02-04 03:11:32 +0530

    Initial commit

--- app/src/main/java/com/example/hyperlocalecom/ResetPasswordFragment.java
+++ app/src/main/java/com/example/hyperlocalecom/ResetPasswordFragment.java
@@ -2,11 +2,26 @@ package com.example.hyperlocalecom;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +34,12 @@ public class ResetPasswordFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private EditText registeredEmail;
+    private Button resetPasswordBtn;
+    private TextView goBAck;
+    private FrameLayout parentFrameLayout;
+    private FirebaseAuth firebaseAuth;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -59,6 +80,90 @@ public class ResetPasswordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reset_password, container, false);
+       View view = inflater.inflate(R.layout.fragment_reset_password, container, false);
+       registeredEmail = view.findViewById(R.id.reset_password_email);
+       resetPasswordBtn = view.findViewById(R.id.reset_password_button);
+       goBAck = view.findViewById(R.id.reset_password_go_back);
+
+       parentFrameLayout = getActivity().findViewById(R.id.registerFrameLayout);
+       firebaseAuth = FirebaseAuth.getInstance();
+       return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        registeredEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                checkInputs();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        resetPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                resetPasswordBtn.setEnabled(false);
+
+                firebaseAuth.sendPasswordResetEmail(registeredEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getActivity(), "Email sent successfully!", Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            String error = task.getException().getMessage();
+                            Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+
+
+                        }
+
+                        resetPasswordBtn.setEnabled(true);
+
+                    }
+                });
+
+            }
+        });
+
+        goBAck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                setFragment(new SignInFragment());
+
+            }
+        });
+    }
+
+    private void checkInputs(){
+        if(TextUtils.isEmpty(registeredEmail.getText())){
+            resetPasswordBtn.setEnabled(false);
+
+        }else{
+            resetPasswordBtn.setEnabled(true);
+
+        }
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_left,R.anim.slideout_from_right);
+        fragmentTransaction.commit();
     }
 }
